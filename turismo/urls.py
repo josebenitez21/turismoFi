166d151d7a3235ccbae45e6880efcd453aea870c
josebenitez21 <jlbenitez4@utpl.edu.ec>
2023-07-24 18:09:26 -0500

    Cambios Proyecto

--- turismo/urls.py
+++ turismo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+
+import EjemploHighcharts.views
 from core import views as core_views
 from EjemploHighcharts import views
 from EjemploHighcharts import views as ejhc_views
@@ -8,18 +10,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', core_views.index, name='index'),
-    path('login/', core_views.login_view, name='login'),
-    path('logout/', core_views.logout_view, name='logout'),
-    path('accounts/profile/', core_views.profile, name='profile'),
-    path('core/', include('core.urls', namespace='core')),
-    path('rest/', include('rest.urls', namespace='rest')),
-    path('obtener-datos/', views.obtener_datos, name='obtener_datos'),
-    path('ejemplohighcharts/', include('EjemploHighcharts.urls')),
-path('estadistica/', TemplateView.as_view(template_name='Estadistica.html'), name='estadistica'),
+                  path('admin/', admin.site.urls),
+                  path('', core_views.index, name='index'),
+                  path('login/', core_views.login_view, name='login'),
+                  path('logout/', core_views.logout_view, name='logout'),
+                  path('accounts/profile/', core_views.profile, name='profile'),
+                  path('core/', include('core.urls', namespace='core')),
+                  path('rest/', include('rest.urls', namespace='rest')),
+                  path('obtener-datos/', views.obtener_datos, name='obtener_datos'),
+                  path('ejemplohighcharts/', include('EjemploHighcharts.urls')),
+                  path('estadistica/', TemplateView.as_view(template_name='Estadistica.html'), name='estadistica'),
 
-
-
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
